89bc0a232eaff92be784ebe202d5944d62045481
Dan Wolfgang <dan@uinnovations.com>
2013-02-06 14:21:23 -0500

    Remove trailing whitespace.

--- plugins/AssetImageQuality/lib/AssetImageQuality/Plugin.pm
+++ plugins/AssetImageQuality/lib/AssetImageQuality/Plugin.pm
@@ -5,34 +5,34 @@ sub quality {
     my $plugin = $cb->plugin;
 
     # We don't want to recompress when the image is uploaded, only when it's
-    # used. When the image is uploaded, it may be sized and compressed just 
-    #perfectly, or it may be too big--we just don't know, so now is the
+    # used. When the image is uploaded, it may be sized and compressed just
+    # perfectly, or it may be too big -- we just don't know, so now is the
     # wrong time to try to recompress.
     return if ($app->mode eq 'upload_file');
 
-    # Look at the $original to determine if this is a new image or an 
-    # existing image. New images can be recompressed. Existing images 
-    # shouldn't be recompressed, because we don't know if they've been 
+    # Look at the $original to determine if this is a new image or an
+    # existing image. New images can be recompressed. Existing images
+    # shouldn't be recompressed, because we don't know if they've been
     # recompressed or how many times they are being recompressed.
     return if $original->column_values->{'id'};
 
     my $id = $obj->column_values->{'id'};
     return unless $id;
-    
-    # A file path should always be returned (right?) but just give up if 
+
+    # A file path should always be returned (right?) but just give up if
     # there isn't one.
     my $asset = MT->model('asset')->load( $id );
     my $file_path = $asset->file_path;
     return unless $file_path;
 
-    # Determine the file type of the image. Give up if it's not a jpeg, 
-    # because that is the only type we can recompress, and the only things 
+    # Determine the file type of the image. Give up if it's not a jpeg,
+    # because that is the only type we can recompress, and the only things
     # that we give the user an opportunity to control.
     my %Types = (jpg => 'jpeg', jpeg => 'jpeg', gif => 'gif', 'png' => 'png');
     my $type = $Types{ lc $asset->file_ext };
     return unless $type =~ /(jpeg)/;
 
-    # This plugin requires ImageMagick. Look for IM before proceeding, 
+    # This plugin requires ImageMagick. Look for IM before proceeding,
     # and give up if it isn't found.
     eval { require Image::Magick; };
     if ($@) {
@@ -43,7 +43,7 @@ sub quality {
         });
         return;
     }
-    
+
     my $image = Image::Magick->new;
     $image->Read(filename => $file_path);
 
@@ -55,11 +55,11 @@ sub quality {
         });
         return;
     }
-    
+
     # Load the user-defined size for this image type.
     my $size = $plugin->get_config_value('compression_quality_'.$type.'_size_threshold');
 
-    # Use the image size (dimensions) to determine if this is a "large" or 
+    # Use the image size (dimensions) to determine if this is a "large" or
     # "small" image. Compare both the asset width and height to decide if
     # its large or small. Then, apply the correct user-supplied compression
     # to the image.
